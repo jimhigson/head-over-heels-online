011a029a2ad9cee5b9661a6f9a2f79521d94ade5
Jim Higson <jimhigson@protonmail.com>
2024-12-11 15:42:08 +0000

    centre bubbles correctly

--- src/game/gameState/mutators/makeItemFadeOut.ts
+++ src/game/gameState/mutators/makeItemFadeOut.ts
@@ -5,6 +5,7 @@ import type { RoomState } from "@/model/modelTypes";
 import type { GameState } from "../GameState";
 import { fadeInOrOutDuration } from "@/game/render/animationTimings";
 import { addItemToRoomInPlay } from "./addItemToRoomInPlay";
+import { addXyz, originXyz, scaleXyz, subXyz } from "@/utils/vectors/vectors";
 
 /**
  * remove an item (with bubbles)
@@ -23,10 +24,21 @@ export const makeItemFadeOut = <RoomId extends string>({
   const bubblesItem = addItemToRoomInPlay({
     itemType: "bubbles",
     config: { style: "white" },
-    position: touchedItem.state.position,
+    // give any placeholder position:
+    position: originXyz,
     room,
     gameState,
   });
+
+  // reposition the bubbles to be centered on the deleted item:
+  const deletedItemCentre = addXyz(
+    touchedItem.state.position,
+    scaleXyz(touchedItem.aabb, 0.5),
+  );
+  bubblesItem.state.position = subXyz(
+    deletedItemCentre,
+    scaleXyz(bubblesItem.aabb, 0.5),
+  );
 
   // remove bubbles after a time:
   bubblesItem.state.expires = gameState.gameTime + fadeInOrOutDuration;
