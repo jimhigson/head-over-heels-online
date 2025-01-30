51eb4b5eb57460172ec9d6c2cff38cf336c8bc69
Jim Higson <jimhigson@protonmail.com>
2025-01-30 16:09:35 +0000

    centre characters under their names on menu

--- src/game/components/dialogs/menuDialog/menus/MainMenuHeading.tsx
+++ src/game/components/dialogs/menuDialog/menus/MainMenuHeading.tsx
@@ -1,11 +1,10 @@
 import { BitmapText } from "../../../Sprite";
 import { mainMenuCycle } from "./mainMenu";
 
-
 export const MainMenuHeading = () => (
   <div className="flex  ml-3">
-    <div className="flex flex-col gap-y-oneScaledPix">
-      <BitmapText className="sprites-double-height text-metallicBlue zx:text-zxYellow me-1">
+    <div className="flex flex-col gap-y-oneScaledPix items-center me-1">
+      <BitmapText className="sprites-double-height text-metallicBlue zx:text-zxYellow">
         Head
       </BitmapText>
       <div className="mt-1 relative">
@@ -16,11 +15,11 @@ export const MainMenuHeading = () => (
     <BitmapText classnameCycle={mainMenuCycle} className="mt-1 me-1">
       over
     </BitmapText>
-    <div className="flex flex-col">
-      <BitmapText className="sprites-double-height text-pink zx:text-zxYellow me-1">
+    <div className="flex flex-col items-center me-1">
+      <BitmapText className="sprites-double-height text-pink zx:text-zxYellow">
         Heels
       </BitmapText>
-      <div className="mt-1 ml-2 relative">
+      <div className="mt-1 relative">
         <span className="sprite texture-heels.walking.towards.2 hover:texture-animated-heels.walking.towards relative z-topSprite" />
         <span className="sprite texture-shadow.smallRound absolute left-0 top-[calc(var(--scale)*1px)] opacity-halfBrite" />
       </div>
