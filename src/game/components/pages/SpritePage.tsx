c235afc32683a2a3a1e126855f080590ee6d7510
Jim Higson <jimhigson@protonmail.com>
2025-02-11 11:51:49 +0000

    fix sprites page

--- src/game/components/pages/SpritePage.tsx
+++ src/game/components/pages/SpritePage.tsx
@@ -1,6 +1,18 @@
 import { useEffect, useState } from "react";
 import { load, spriteSheet } from "../../../sprites/spriteSheet";
-import type { TextureId } from "../../../sprites/spriteSheetData";
+import {
+  spritesheetData,
+  type TextureId,
+} from "../../../sprites/spriteSheetData";
+
+declare module "react" {
+  interface CSSProperties {
+    "--x"?: string | number;
+    "--y"?: string | number;
+    "--h"?: string | number;
+    "--w"?: string | number;
+  }
+}
 
 export const SpritePage = () => {
   const [loaded, setLoaded] = useState<boolean>(false);
@@ -25,7 +37,16 @@ export const SpritePage = () => {
         return (
           <div key={textureId} className="bg-shadow m-[8px] p-[16px]">
             <div
-              className={`sprite texture-${textureId} bg-shadow hover:bg-metallicBlue hover:border-pink border-shadow border border-4 mb-1 box-content`}
+              className={`sprite bg-shadow hover:bg-metallicBlue hover:border-pink border-shadow border border-4 mb-1 box-content`}
+              // most of these textures won't have classes loaded by tailwid,
+              // so inline the relevant info - including by tailwind would inflate
+              // the css in prod
+              style={{
+                "--x": `${spritesheetData.frames[textureId].frame.x}px`,
+                "--y": `${spritesheetData.frames[textureId].frame.y}px`,
+                "--h": `${spritesheetData.frames[textureId].frame.h}px`,
+                "--w": `${spritesheetData.frames[textureId].frame.w}px`,
+              }}
             />
             <div className="text-moss">{textureId}</div>
             <div className="text-lightGrey">
