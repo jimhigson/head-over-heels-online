82281a7eb12a5d95497f65efb378e19d823a9f87
Jim Higson <jim@blockstack.ing>
2025-03-03 13:32:28 +0000

    allow 512 icon

--- vite.config.ts
+++ vite.config.ts
@@ -27,10 +27,17 @@ export default defineConfig({
         short_name: "Head over Heels",
         orientation: "landscape",
         display: "fullscreen",
+        scope: "/",
+        start_url: "/",
         icons: [
           {
             src: "/icon-192.png",
             sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
             type: "image/png",
           },
         ],
