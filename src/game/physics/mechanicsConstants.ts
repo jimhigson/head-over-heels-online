39fa6822aa8cfadbdd4a5032f05685ddc02fee19
Jim Higson <jim@blockstack.ing>
2025-03-14 10:54:16 +0000

    correction to enemy speed

--- src/game/physics/mechanicsConstants.ts
+++ src/game/physics/mechanicsConstants.ts
@@ -54,6 +54,11 @@ export const moveSpeedPixPerMs = {
   charles: onePxPerFrameInOriginalGamePxPerMs,
   // moved diagonally in a (1, 1) vector in original - that gives us √2 px per frame
   dalek: Math.SQRT2 * onePxPerFrameInOriginalGamePxPerMs,
+  // moved at different speeds in original depending on direction:
+  //    * orthogonally eg (0,1)
+  //    * diagonally in a (1, 1) vector  -
+  //    that gives us 1 or √2 px per frame - take the average:
+  bubbleRobot: ((Math.SQRT2 + 1) / 2) * onePxPerFrameInOriginalGamePxPerMs,
   cyberman: 1 * onePxPerFrameInOriginalGamePxPerMs,
   skiHead: onePxPerFrameInOriginalGamePxPerMs,
   helicopterBug: onePxPerFrameInOriginalGamePxPerMs,
@@ -65,7 +70,7 @@ export const moveSpeedPixPerMs = {
   // needs to be quite fast, or he is possible to run around using
   // analogue control. This seems to be fast enough.
   emperorsGuardian: Math.SQRT2 * onePxPerFrameInOriginalGamePxPerMs,
-  bubbleRobot: onePxPerFrameInOriginalGamePxPerMs,
+
   computerBot: onePxPerFrameInOriginalGamePxPerMs,
   turtle: onePxPerFrameInOriginalGamePxPerMs,
   ball: 2 * onePxPerFrameInOriginalGamePxPerMs,
