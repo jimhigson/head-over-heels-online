d9ff7131fd1bf31a80161105c184522a3a005337
Jim Higson <jim@blockstack.ing>
2025-04-14 21:32:33 -0700

    refactor and enhance room rendering on the map

--- src/game/components/dialogs/menuDialog/dialogs/map/mapConstants.tsx
+++ src/game/components/dialogs/menuDialog/dialogs/map/mapConstants.tsx
@@ -1,6 +1,6 @@
 export const roomGridSizeXY = 48;
 export const roomGridSizeZ = 36;
-export const doorwayGap = 0.6;
+export const doorwayGap = 0.55;
 export const roomFront = roomGridSizeXY * ((1 - doorwayGap) / 2);
 export const roomBack = roomGridSizeXY - roomFront;
 export const mapSvgMargin = 8;
