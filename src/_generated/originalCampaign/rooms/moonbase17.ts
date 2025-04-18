494fcc4b648c452b3e5fd97d3578466d0a3c2c7d
Jim Higson <jim@blockstack.ing>
2025-04-18 06:54:13 +0100

    rearrange map (#227)

--- src/_generated/originalCampaign/rooms/moonbase17.ts
+++ src/_generated/originalCampaign/rooms/moonbase17.ts
@@ -65,7 +65,7 @@ export const room = inferRoomJson({
   },
   meta: {
     nonContiguousRelationship: {
-      gridOffset: { x: -4, y: -3, z: 0 },
+      gridOffset: { x: 2, y: 4, z: 0 },
       with: { room: "moonbase18" },
     },
   },
