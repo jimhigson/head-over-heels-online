9a818430416b4a2b60327f2bcd2dd8200d52ab86
Jim Higson <jim@blockstack.ing>
2025-05-07 14:36:04 +0100

    conveyors slow down, not instantly stop (#260)

--- src/game/render/itemAppearances/conveyorAppearance.ts
+++ src/game/render/itemAppearances/conveyorAppearance.ts
@@ -1,58 +1,97 @@
-import { Container } from "pixi.js";
+import type { AnimatedSprite } from "pixi.js";
+import type { Container } from "pixi.js";
 import { isStoodOn } from "../../../model/StoodOnBy";
 import { directionAxis } from "../../../utils/vectors/vectors";
 import { createSprite } from "../createSprite";
 import type { ItemAppearance } from "./ItemAppearance";
+import { spritesheetData } from "../../../sprites/spriteSheetData";
+import { neverTime } from "../../../utils/veryClose";
+import { itemAppearanceOutsideView } from "./itemAppearanceOutsideView";
+
+const slowdownTimeMs = 500;
 
 type ConveyorRenderProps = {
   moving: boolean;
+  roomTimeStoppedMoving?: number;
 };
 
-export const conveyorAppearance: ItemAppearance<
+const conveyorAnimationSpeed =
+  spritesheetData.animations["conveyor.x"].animationSpeed;
+const frameCount = spritesheetData.animations["conveyor.x"].length;
+
+const easeOut = (t: number): number => 1 - (1 - t) ** 2;
+
+/**
+ * staggering the animation frames of multiple-sprite conveyors
+ * gives a 'wave' effect in the direction of motion
+ */
+const staggerAnimation = (
+  rendering: Container<AnimatedSprite>,
+  reverse: boolean,
+) => {
+  for (let i = 0; i < rendering.children.length; i++) {
+    const c = rendering.children[i];
+    const frame = i % frameCount;
+    c.gotoAndStop(reverse ? frameCount - frame - 1 : frame);
+  }
+
+  return rendering;
+};
+
+const conveyorAppearanceImpl: ItemAppearance<
   "conveyor",
-  ConveyorRenderProps
+  ConveyorRenderProps,
+  Container<AnimatedSprite>
 > = ({
   renderContext: {
     item: {
       config: { direction, times },
       state: { stoodOnBy },
     },
-    paused,
+    room: { roomTime },
   },
   currentRendering,
 }) => {
   const currentlyRenderedProps = currentRendering?.renderProps;
   const moving = isStoodOn(stoodOnBy);
 
-  const render =
-    currentlyRenderedProps === undefined ||
-    currentlyRenderedProps.moving !== moving;
-
-  if (!render) {
-    return "no-update";
-  }
-
-  const rendering = new Container();
+  const roomTimeStoppedMoving =
+    (!moving && currentlyRenderedProps?.moving ?
+      roomTime
+    : currentlyRenderedProps?.roomTimeStoppedMoving) ?? neverTime;
 
   const axis = directionAxis(direction);
-  rendering.addChild(
-    createSprite(
-      moving ?
-        {
-          animationId: `conveyor.${axis}`,
-          reverse: direction === "towards" || direction === "right",
-          times,
-          paused,
-        }
-      : {
-          textureId: `conveyor.${axis}.6`,
-          times,
-        },
-    ),
-  );
+  const rendering =
+    currentRendering?.output ??
+    staggerAnimation(
+      createSprite({
+        animationId: `conveyor.${axis}`,
+        reverse: direction === "towards" || direction === "right",
+        times,
+      }),
+      direction === "towards" || direction === "right",
+    );
+
+  const periodSinceStopped =
+    moving ? 0 : Math.min(roomTime - roomTimeStoppedMoving, slowdownTimeMs);
+  const playSpeedFrac = Math.max(0, 1 - periodSinceStopped / slowdownTimeMs);
+
+  for (const c of rendering.children) {
+    if (playSpeedFrac === 0) {
+      c.stop();
+    } else {
+      const playSpeed = conveyorAnimationSpeed * easeOut(playSpeedFrac);
+      c.play();
+      c.animationSpeed = playSpeed;
+    }
+  }
 
   return {
     output: rendering,
-    renderProps: { moving },
+    renderProps: { moving, roomTimeStoppedMoving },
   };
 };
+
+export const conveyorAppearance = itemAppearanceOutsideView(
+  conveyorAppearanceImpl,
+);
