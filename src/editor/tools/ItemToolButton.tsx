48eb86299708d1a72abfd3b0e52fbc5a8ae58e49
Jim Higson <jim@blockstack.ing>
2025-06-07 21:50:06 +0100

    level editor toolbar restructure (#287)
    * simpler icons in toolbars
    
    * more compact toolbar

--- src/editor/tools/ItemToolButton.tsx
+++ src/editor/tools/ItemToolButton.tsx
@@ -1,10 +1,5 @@
-import type { Simplify } from "type-fest";
-import type { JsonItemType, JsonItemConfig } from "../../model/json/JsonItem";
-import type { MonsterWhich } from "../../model/json/MonsterJsonConfig";
-import type { BlockStyle } from "../../model/json/utilityJsonConfigTypes";
+import type { JsonItemType } from "../../model/json/JsonItem";
 import { Button } from "../../ui/button";
-import { twClass } from "../twClass";
-import type { EditorRoomId, EditorRoomItemId } from "../EditorRoomId";
 import { store } from "../../store/store";
 import {
   selectTool,
@@ -13,89 +8,18 @@ import {
 } from "../slice/levelEditorSlice";
 import type { ItemTool } from "../Tool";
 import nanoEqual from "nano-equal";
-import type {
-  DeadlyBlockStyle,
-  PortableBlockStyle,
-} from "../../model/json/ItemConfigMap";
 
-const itemTypeToButtonIconClasses: Simplify<
-  Partial<
-    Record<
-      | JsonItemType
-      | `monster[which=${MonsterWhich}]`
-      | `block[style=${BlockStyle}]`
-      | `portableBlock[style=${PortableBlockStyle}]`
-      | `pushableBlock[style=${PortableBlockStyle}]`
-      | `deadlyBlock[style=${DeadlyBlockStyle}]`,
-      string
-    >
-  >
-> = twClass({
-  door: "sprite texture-door.frame.generic.x.whole",
-  ball: "sprite texture-ball",
-  teleporter: "sprite texture-teleporter",
-  conveyor:
-    "sprite texture-conveyor.y.1 [button:hover_&]:texture-animated-conveyor.x",
-  lift: "sprite texture-lift.static [button:hover_&]:texture-animated-lift",
-  barrier: "sprite texture-barrier.x",
-  pickup: "sprite texture-whiteRabbit",
-  "portableBlock[style=cube]": "sprite texture-cube",
-  "pushableBlock[style=stepStool]": "sprite texture-stepStool",
-  "movingPlatform[style=sandwich]": "sprite texture-sandwich",
-  charles:
-    "sprite texture-charles.towards [button:hover_&]:texture-charles.right",
-  joystick: "sprite texture-joystick",
-  switch: `sprite texture-switch.left [button:hover_&]:texture-switch.right`,
-  spikes: `sprite texture-spikes`,
-  "deadlyBlock[style=volcano]":
-    "sprite texture-volcano.1 [button:hover_&]:texture-animated-volcano",
-  slidingDeadly: `sprite texture-spikyBall.1 [button:hover_&]:texture-spikyBall.2`,
-  "monster[which=turtle]":
-    "sprite texture-turtle.towards.1 [button:hover_&]:texture-animated-turtle.right",
-  "monster[which=dalek]":
-    "sprite texture-dalek.1 [button:hover_&]:texture-animated-dalek",
-  "monster[which=elephantHead]":
-    "sprite texture-elephant.towards [button:hover_&]:texture-elephant.right",
-  "monster[which=skiHead]":
-    "sprite texture-skiHead.greenAndPink.towards [button:hover_&]:texture-skiHead.greenAndPink.right",
-  "monster[which=cyberman]":
-    "sprite texture-cyberman.towards [button:hover_&]:texture-cyberman.right",
-  "block[style=organic]": `sprite texture-block.organic`,
-  "block[style=artificial]": `sprite texture-block.artificial`,
-  "block[style=book]": `sprite texture-book.x`,
-});
+import type { PropsWithChildren } from "react";
 
 type ItemToolButtonProps<T extends JsonItemType> = {
   itemTool: ItemTool<T>;
 };
 
-const getIconClasses = <T extends JsonItemType>(
-  itemTool: ItemTool<T>,
-): string | undefined => {
-  if (itemTool.type === "monster") {
-    return itemTypeToButtonIconClasses[
-      `monster[which=${(itemTool.config as JsonItemConfig<"monster", EditorRoomId, EditorRoomItemId>).which}]`
-    ];
-  } else if (
-    itemTool.type === "block" ||
-    itemTool.type === "portableBlock" ||
-    itemTool.type === "deadlyBlock" ||
-    itemTool.type === "movingPlatform" ||
-    itemTool.type === "pushableBlock"
-  ) {
-    return itemTypeToButtonIconClasses[
-      `${itemTool.type}[style=${(itemTool.config as JsonItemConfig<"block", EditorRoomId, EditorRoomItemId>).style}]`
-    ];
-  }
-  return itemTypeToButtonIconClasses[itemTool.type];
-};
-
 export const ItemToolButton = <T extends JsonItemType>({
   itemTool,
-}: ItemToolButtonProps<T>) => {
+  children,
+}: PropsWithChildren<ItemToolButtonProps<T>>) => {
   const currentTool = useAppSelectorWithLevelEditorSlice(selectTool);
-
-  const iconClasses = getIconClasses(itemTool);
 
   const isCurrentTool = nanoEqual(
     currentTool?.type === "item" && currentTool.item,
@@ -103,12 +27,10 @@ export const ItemToolButton = <T extends JsonItemType>({
   );
   return (
     <Button
-      className={`flex-1 h-min p-quarter grow-0 ${isCurrentTool ? "bg-pastelBlue" : ""}`}
+      className={`h-3 w-3 overflow-hidden ${isCurrentTool ? "bg-pastelBlue" : ""}`}
       onClick={() => store.dispatch(setTool({ type: "item", item: itemTool }))}
     >
-      <span
-        className={`${iconClasses} relative [button:active_&]:top-oneScaledPix`}
-      />
+      {children}
     </Button>
   );
 };
