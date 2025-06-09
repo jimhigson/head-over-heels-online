22ce9b19cbd4ba10fb489d28d9bea94cb12bcdd5
Jim Higson <jim@blockstack.ing>
2025-06-09 21:24:43 +0100

    placing doors cuts holes in walls

--- src/editor/RoomEditingArea/cursor/itemToolPutDownLocation.tsx
+++ src/editor/RoomEditingArea/cursor/itemToolPutDownLocation.tsx
@@ -17,7 +17,13 @@ import type {
   JsonItemType,
   JsonItemUnion,
 } from "../../../model/json/JsonItem";
-import { addXyz, type Xyz } from "../../../utils/vectors/vectors";
+import type { Xy } from "../../../utils/vectors/vectors";
+import {
+  addXyz,
+  directionAxis,
+  perpendicularAxisXy,
+  type Xyz,
+} from "../../../utils/vectors/vectors";
 import { unitVectors } from "../../../utils/vectors/unitVectors";
 import {
   doorOverallWidthPx,
@@ -46,36 +52,45 @@ export const itemToolPutDownLocation = (
     if (pointingAtItem.type !== "wall") {
       return undefined;
     }
-    const clampedPosition: Xyz = {
-      x: Math.max(
-        Math.min(
-          pointingAt.position.x,
-          pointingAtItem.state.position.x +
-            pointingAtItem.aabb.x -
-            doorOverallWidthPx,
-        ),
-        pointingAtItem.state.position.x,
-      ),
-      y: Math.max(
-        Math.min(
-          pointingAt.position.y,
-          pointingAtItem.state.position.y +
-            pointingAtItem.aabb.y -
-            doorOverallWidthPx,
-        ),
-        pointingAtItem.state.position.y,
-      ),
+    const {
+      config: wallConfig,
+      aabb: wallAabb,
+      state: { position: wallPosition },
+    } = pointingAtItem;
+
+    const currentWallTimes: Xy = { x: 1, y: 1, ...wallConfig.times };
+
+    /** axis running along the wall the door sits on */
+    const alongWallAxis = perpendicularAxisXy(
+      directionAxis(wallConfig.direction),
+    );
+    /** axis for direction of travel through the doorway */
+    const doorDirectionAxis = directionAxis(wallConfig.direction);
+
+    if (currentWallTimes[alongWallAxis] < 2) {
+      return undefined; // wall not big enough for a door
+    }
+
+    const alongMin = wallPosition[alongWallAxis];
+    const alongMax =
+      wallPosition[alongWallAxis] +
+      wallAabb[alongWallAxis] -
+      doorOverallWidthPx;
+
+    const zMin = wallPosition.z;
+    const zMax =
       // door can't go over the top of the wall:
-      z: Math.max(
-        Math.min(
-          pointingAt.position.z,
-          pointingAtItem.state.position.z +
-            pointingAtItem.aabb.z -
-            doorPostHeightPx,
-        ),
-        pointingAtItem.state.position.z,
+      wallPosition.z + wallAabb.z - doorPostHeightPx;
+
+    const clampedPosition = {
+      [alongWallAxis]: Math.max(
+        Math.min(pointingAt.position[alongWallAxis], alongMax),
+        alongMin,
       ),
-    };
+      [doorDirectionAxis]: pointingAt.position[doorDirectionAxis],
+      z: Math.max(Math.min(pointingAt.position.z, zMax), zMin),
+    } as Xyz;
+
     return fineXyzToBlockXyz(clampedPosition);
   }
 
@@ -106,18 +121,14 @@ export const previewItemsForCursor = <T extends JsonItemType>(
   const loadItems = <T2 extends JsonItemType>(
     type: T2,
     config: JsonItemConfig<T2, EditorRoomId, EditorRoomItemId>,
+    position: Xyz,
   ) => {
-    const jsonBlockPosition = itemToolPutDownLocation(
-      pointingAt,
-      roomState,
-      itemTool,
-    );
     const loadedItems = loadItemFromJson(
       `cursor/${cursorItemCount++}`,
       {
         type,
         config,
-        position: jsonBlockPosition,
+        position,
       } as JsonItemUnion<string, string>,
       roomState.roomJson,
     ).toArray() as EditorUnionOfAllItemInPlayTypes[];
@@ -129,6 +140,16 @@ export const previewItemsForCursor = <T extends JsonItemType>(
     return loadedItems;
   };
 
+  const jsonBlockPosition = itemToolPutDownLocation(
+    pointingAt,
+    roomState,
+    itemTool,
+  );
+
+  if (jsonBlockPosition === undefined) {
+    return undefined; // can't put down here
+  }
+
   if (itemTool.type === "door") {
     const pointingAtItem = roomState.items[pointingAt.itemId];
 
@@ -138,28 +159,33 @@ export const previewItemsForCursor = <T extends JsonItemType>(
 
     const wallDirection = pointingAtItem.config.direction;
 
-    const loadedItems = loadItems<"door">("door", {
-      ...(itemTool.config as JsonItemConfig<
-        "door",
-        EditorRoomId,
-        EditorRoomItemId
-      >),
-      direction: wallDirection,
-    });
+    const loadedItems = loadItems<"door">(
+      "door",
+      {
+        ...(itemTool.config as JsonItemConfig<
+          "door",
+          EditorRoomId,
+          EditorRoomItemId
+        >),
+        direction: wallDirection,
+      },
+      jsonBlockPosition,
+    );
 
     // doors are a special case since they can only be added on walls:
     return loadedItems;
-  } else {
-    const loadedItems = loadItems(itemTool.type, itemTool.config);
-
-    const hasCollisions = loadedItems.some((loadedItem) => {
-      const collisions = collision1toManyIter(
-        loadedItem,
-        collideableItemsInRoom,
-      );
-
-      return !isEmpty(collisions);
-    });
-    return hasCollisions ? undefined : loadedItems;
   }
+
+  const loadedItems = loadItems(
+    itemTool.type,
+    itemTool.config,
+    jsonBlockPosition,
+  );
+
+  const hasCollisions = loadedItems.some((loadedItem) => {
+    const collisions = collision1toManyIter(loadedItem, collideableItemsInRoom);
+
+    return !isEmpty(collisions);
+  });
+  return hasCollisions ? undefined : loadedItems;
 };
---
+++ src/editor/slice/reducers/applyToolToRoomJson.test.ts
@@ -0,0 +1,5 @@
+import { test } from "vitest";
+
+test("can cut a hole in a wall, making it into two walls", () => {});
+test("move the start of a wall", () => {});
+test("move the end of a wall", () => {});
