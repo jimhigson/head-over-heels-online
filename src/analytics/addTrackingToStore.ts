7007688be1c507aaede74f51cbbcd2d18496e327
Jim Higson <jim@blockstack.ing>
2025-08-17 22:27:05 +0100

    add some events to umami.is analytics (#422)

---
+++ src/analytics/addTrackingToStore.ts
@@ -0,0 +1,85 @@
+import { startAppListening } from "../store/listenerMiddleware";
+import { isAnyOf } from "@reduxjs/toolkit";
+import {
+  characterRoomChange,
+  crownCollected,
+  errorCaught,
+  gameOver,
+  gameStarted,
+  lostLife,
+  reincarnationAccepted,
+} from "../store/slices/gameMenusSlice";
+import { useEffect } from "react";
+import { useMaybeGameApi } from "../game/components/GameApiContext";
+import { HookComponent } from "../utils/react/HookComponent";
+
+const isTrackedEvent = isAnyOf(
+  gameOver,
+  reincarnationAccepted,
+  // roomExplored is almost the same as characterRoomChange, but has less information
+  //roomExplored,
+  characterRoomChange,
+  lostLife,
+  errorCaught,
+  gameStarted,
+  crownCollected,
+);
+
+const useAddTrackingToStore = () => {
+  const maybeGameApi = useMaybeGameApi();
+
+  useEffect(() => {
+    const unsub = startAppListening({
+      matcher: isTrackedEvent,
+      effect(action, { getState }) {
+        const gameState = maybeGameApi?.gameState;
+
+        const [, actionNameWithoutSlice] = action.type.split("/");
+        const state = getState();
+
+        const globalTimeSeconds =
+          gameState ? Math.round(gameState.gameTime / 1000) : undefined;
+
+        const payloadProperties =
+          action.payload === undefined ? {}
+          : action.type === "gameMenus/crownCollected" ?
+            { crownForPlanet: action.payload }
+          : action.type === "gameMenus/gameStarted" ? undefined
+          : action.type === "gameMenus/lostLife" ?
+            {
+              position: action.payload.characterLosingLifeItem.state.position,
+            }
+          : action.payload;
+
+        const eventProperties = {
+          gameTimeSeconds: globalTimeSeconds,
+          ...state.gameMenus.gameInPlay,
+          cheatsOn: state.gameMenus.cheatsOn,
+          ...payloadProperties,
+          currentCharacter: gameState?.currentCharacterName,
+          inRoom: gameState?.characterRooms[gameState.currentCharacterName],
+        };
+
+        if (window.umami) {
+          console.debug(
+            "🍜 umami event:",
+            actionNameWithoutSlice,
+            eventProperties,
+          );
+          window.umami.track(actionNameWithoutSlice, eventProperties);
+          // didn't load umami - maybe browser is offline
+        } else {
+          console.debug(
+            "🍜 no umami for event:",
+            actionNameWithoutSlice,
+            eventProperties,
+          );
+        }
+      },
+    });
+
+    return unsub;
+  }, [maybeGameApi]);
+};
+
+export const AddTrackingToStore = HookComponent(useAddTrackingToStore);
