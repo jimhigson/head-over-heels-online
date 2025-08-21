8203b50a037e76e9a59f186fbc49990f10f62d1b
Jim Higson <jim@blockstack.ing>
2025-08-21 23:22:34 +0100

    change location of github link on main screen (#433)

--- src/game/components/dialogs/menuDialog/dialogs/mainMenu/GitRepoInfo.tsx
+++ src/game/components/dialogs/menuDialog/dialogs/mainMenu/GitRepoInfo.tsx
@@ -8,29 +8,36 @@ export const GitRepoInfo = () => {
   const [open, toggleOpen] = useReducer((o: boolean) => !o, false);
 
   return (
-    <div className="flex  justify-between absolute bottom-oneScaledPix right-2 left-2 z-dialog">
-      <div onClick={toggleOpen} className="text-metallicBlue zx:text-zxMagenta">
-        {!open && <BitmapText>v</BitmapText>}
-        {open && (
-          <BitmapText className="bg-metallicBlueHalfbrite block">
-            {__gitHash__ || ""}
-            {__buildDate__ || ""}
+    <>
+      <div className="flex absolute top-oneScaledPix right-1 z-dialog">
+        <a
+          href={repoLocation}
+          className="bitmap-text-link bg-transparent zx:bg-zxBlack"
+        >
+          <BitmapText className="text-highlightBeige zx:text-zxYellow">
+            ★
           </BitmapText>
-        )}
+          <span className="text-lightGrey zx:text-whiteHalfbrite">
+            <BitmapText className="resHandheld:hidden"> on</BitmapText>
+            <BitmapText>{nerdFontGithubChar}</BitmapText>
+            <BitmapText className="resHandheld:hidden">github</BitmapText>
+          </span>
+        </a>
       </div>
-      <a
-        href={repoLocation}
-        className="bitmap-text-link bg-transparent zx:bg-zxBlack"
-      >
-        <BitmapText className="text-highlightBeige zx:text-zxYellow">
-          ★
-        </BitmapText>
-        <span className="text-lightGrey zx:text-whiteHalfbrite">
-          <BitmapText className="resHandheld:hidden"> on</BitmapText>
-          <BitmapText>{nerdFontGithubChar}</BitmapText>
-          <BitmapText className="resHandheld:hidden">github</BitmapText>
-        </span>
-      </a>
-    </div>
+      <div className="flex absolute bottom-oneScaledPix right-oneScaledPix z-dialog">
+        <div
+          onClick={toggleOpen}
+          className="text-metallicBlue zx:text-zxMagenta"
+        >
+          {!open && <BitmapText>v</BitmapText>}
+          {open && (
+            <BitmapText className="bg-metallicBlueHalfbrite block">
+              {__gitHash__ || ""}
+              {__buildDate__ || ""}
+            </BitmapText>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
