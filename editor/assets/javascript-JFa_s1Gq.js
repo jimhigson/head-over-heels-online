2544e86ed7675a9cb2974357cd449a166f5f4cb8
jimhigson <jimhigson@users.noreply.github.com>
2025-08-24 15:56:09 +0000

    deploy: fbb810bab504f75fc2ad9c1a825f6266198fa8bc

--- editor/assets/javascript-JFa_s1Gq.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript-ZwOZkzoy.js";import"./JsonRoomEditor-efDhyLGJ.js";import"./index-YgNRftU0.js";import"./levelEditorSlice-B0gNLrEI.js";import"./RoomJson-G2Ly8HBy.js";import"./LevelEditor-Dm5sBmn4.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
