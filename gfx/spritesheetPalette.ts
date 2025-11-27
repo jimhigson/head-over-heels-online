620eaea0d2950e35dc2b3eb4bcb52345cac34598
Jim Higson <jim@blockstack.ing>
2025-11-27 21:00:00 +0000

    feat: make main palette slightly more saturated and contrasty

--- gfx/spritesheetPalette.ts
+++ gfx/spritesheetPalette.ts
@@ -2,7 +2,7 @@ import { Color } from "pixi.js";
 // this file is generated from the spritesheet by iff2png.sh, do not edit directly
 export const spritesheetPalette = {
   pureBlack: new Color("#000000"),
-  shadow: new Color("#264136"),
+  shadow: new Color("#31413B"),
   midGrey: new Color("#7C6E6C"),
   lightGrey: new Color("#C0AFA9"),
   white: new Color("#FFFFFF"),
@@ -10,10 +10,10 @@ export const spritesheetPalette = {
   metallicBlue: new Color("#226FD0"),
   pink: new Color("#EE7EE6"),
   moss: new Color("#AD9E00"),
-  redShadow: new Color("#8E5944"),
-  midRed: new Color("#EA684F"),
-  lightBeige: new Color("#EE9F7F"),
-  highlightBeige: new Color("#FCC274"),
+  redShadow: new Color("#8E533C"),
+  midRed: new Color("#E25D43"),
+  lightBeige: new Color("#FF9C74"),
+  highlightBeige: new Color("#FFCA97"),
   alpha: new Color("#BBB7E5"),
   replaceLight: new Color("#0D99A1"),
   replaceDark: new Color("#115F60"),
