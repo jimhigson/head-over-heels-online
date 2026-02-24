092e7eb100e9a3cd17c898a95b2ca0f75b3816c5
Jim Higson <jim@blockstack.ing>
2026-02-24 21:00:00 +0000

    fix crash on entering playtest mode from editor

--- src/game/gameState/saving/dispatchSaveGame.ts
+++ src/game/gameState/saving/dispatchSaveGame.ts
@@ -2,6 +2,7 @@ import type { AppStore } from "../../../store/store";
 import type { GameState } from "../GameState";
 
 import { saveGame } from "../../../store/slices/gameMenus/gameMenusSlice";
+import { isInPlaytestMode } from "../../isInPlaytestMode";
 import { createSavedGame } from "./createSavedGame";
 
 /**
@@ -11,8 +12,14 @@ import { createSavedGame } from "./createSavedGame";
  * It is especially important to save in Tauri, since closing the window saves,
  * but exiting the app does not have enough time to save (on mac, cmd-w saves, but
  * cmd-q doesn't). Saving frequently reduces the rewind after app quitting.
+ *
+ * No-ops in playtest mode since playtest state should not be persisted.
  */
 export const dispatchSaveGame = (gameState: GameState, store: AppStore) => {
+  if (isInPlaytestMode()) {
+    return;
+  }
+
   const savedGame = createSavedGame(gameState, store.getState());
   const saveGameAction = saveGame(savedGame);
   store.dispatch(saveGameAction);
