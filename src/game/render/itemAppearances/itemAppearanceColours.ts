c955ced9c1d3894857e2e05a5186cc483a91addd
Jim Higson <jim@blockstack.ing>
2026-04-23 21:31:48 +0100

    refactor: clean out unused code

--- src/game/render/itemAppearances/itemAppearanceColours.ts
+++
@@ -1,3 +0,0 @@
-import { paletteBlockstack } from "../../../sprites/palette/spritesheetPalette";
-
-export const carryableOutlineColour = paletteBlockstack.moss;
--- src/utils/always.tsx
+++
@@ -1,2 +0,0 @@
-export const always = () => true;
-export const never = () => false;
