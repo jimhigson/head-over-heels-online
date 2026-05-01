58e373b528624f9a9c659c539301e869000744e8
Jim Higson <jim@blockstack.ing>
2026-05-01 22:08:44 +0100

    feat: add menu item to clear all data (#836)

--- src/game/components/dialogs/menuDialog/dialogs/options/ClearAllDataMenuSection.tsx
+++ src/game/components/dialogs/menuDialog/dialogs/options/ClearAllDataMenuSection.tsx
@@ -1,28 +1,69 @@
+import { useCallback, useEffect, useState } from "react";
+
+import type { TextureTailwindClass } from "../../../../../../sprites/spritesheet/spritesheetData/TextureTailwindClass";
+
+import { useAppDispatch } from "../../../../../../store/hooks";
+import { startAppListening } from "../../../../../../store/listenerMiddleware";
 import { clearAllData } from "../../../../../../store/slices/clearAllData";
-import { useDispatchActionCallback } from "../../../../../../store/useDispatchActionCallback";
+import { setFocussedMenuItemId } from "../../../../../../store/slices/gameMenus/gameMenusSlice";
 import { BlockyMarkdown } from "../../../../BlockyMarkdown";
-import { optionsHintMarkdownClassname } from "../../../dialogClasses";
+import {
+  optionsHintMarkdownClassname,
+  spriteLeaderClasses,
+} from "../../../dialogClasses";
 import { MenuItem } from "../../MenuItem";
 
 const clearAllDataMarkdown = `##Rest all data
 
 Wipes saved games, key bindings, display and sound preferences, and all other state. **Cannot be undone.**`;
 
-export const ClearAllDataMenuSection = () => (
-  <>
-    <div className="col-span-3 pb-1 mt-3">
-      <BlockyMarkdown
-        markdown={clearAllDataMarkdown}
-        className={optionsHintMarkdownClassname}
+const menuItemId = "clearAllData";
+
+export const ClearAllDataMenuSection = () => {
+  const [awaitingConfirmation, setAwaitingConfirmation] = useState(false);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const unsubscribe = startAppListening({
+      predicate: (action) =>
+        setFocussedMenuItemId.match(action) &&
+        action.payload.focussedItemId !== menuItemId,
+      effect() {
+        setAwaitingConfirmation(false);
+      },
+    });
+    return unsubscribe;
+  }, []);
+
+  const handleSelect = useCallback(() => {
+    if (awaitingConfirmation) {
+      dispatch(clearAllData());
+    } else {
+      setAwaitingConfirmation(true);
+    }
+  }, [awaitingConfirmation, dispatch]);
+
+  return (
+    <>
+      <div className="col-span-3 pb-1 mt-3">
+        <BlockyMarkdown
+          markdown={clearAllDataMarkdown}
+          className={optionsHintMarkdownClassname}
+        />
+      </div>
+      <MenuItem
+        hintInline
+        className="sprites-double-height"
+        leader={
+          <span
+            className={`${spriteLeaderClasses} sprite ${"texture-bubbles_white_1" satisfies TextureTailwindClass} sprites-normal-height selectedMenuItem:texture-animated-bubbles_white zx:sprite-revert-to-white`}
+          />
+        }
+        verticalAlignItemsCentre
+        id={menuItemId}
+        label={awaitingConfirmation ? "Press again to confirm" : "Reset"}
+        onSelect={handleSelect}
       />
-    </div>
-    <MenuItem
-      hintInline
-      className="sprites-double-height"
-      verticalAlignItemsCentre
-      id="clearAllData"
-      label="Reset"
-      onSelect={useDispatchActionCallback(clearAllData)}
-    />
-  </>
-);
+    </>
+  );
+};
