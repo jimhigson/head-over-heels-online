3d907499d812c7e9ce522f682c012015b16ea3e7
Jim Higson <jim@blockstack.ing>
2026-05-20 21:00:00 +0100

    editor: more efficient edit rendering of game preview

---
+++ src/editor/RoomEditingArea/EditorRoomStateProvider.tsx
@@ -0,0 +1,29 @@
+import { useRef } from "preact/hooks";
+import { createContext, type PropsWithChildren, useContext } from "react";
+import { type EmptyObject } from "type-fest";
+
+import { type EditorRoomJson, type EditorRoomState } from "../editorTypes";
+
+type EditorRoomStateRefs = {
+  roomStateRef: { current: EditorRoomState | undefined };
+  prevRoomJsonRef: { current: EditorRoomJson | undefined };
+};
+
+const EditorRoomStateContext = createContext<EditorRoomStateRefs>(
+  null as unknown as EditorRoomStateRefs,
+);
+
+export const EditorRoomStateProvider = ({
+  children,
+}: PropsWithChildren<EmptyObject>) => {
+  const roomStateRef = useRef<EditorRoomState | undefined>(undefined);
+  const prevRoomJsonRef = useRef<EditorRoomJson | undefined>(undefined);
+
+  return (
+    <EditorRoomStateContext.Provider value={{ roomStateRef, prevRoomJsonRef }}>
+      {children}
+    </EditorRoomStateContext.Provider>
+  );
+};
+
+export const useEditorRoomStateRefs = () => useContext(EditorRoomStateContext);
