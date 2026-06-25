711b7cce4dcf6b23efc9857da54de5f3f3df3046
Jim Higson <jim@blockstack.ing>
2026-06-25 17:54:29 +0100

    feat: monaco text editor in hoh editor more customised to look like the rest of the app (#959)

--- src/editor/main.tsx
+++ src/editor/main.tsx
@@ -1,19 +1,19 @@
 import { render } from "preact";
 
 import "./index.css";
+import "./JsonRoomEditor/codiconFontOverrides.css";
+import "./JsonRoomEditor/monacoTooltip.css";
 
 import { Provider } from "react-redux";
 
 import { loadHeadOverHeelsFont } from "../sprites/loadHeadOverHeelsFont";
 import { store } from "../store/store";
-import { loadCodiconFont } from "./JsonRoomEditor/loadCodiconFont";
 import { LevelEditorLoader } from "./LevelEditorLoader";
 import { registerRoomPreviewSnapshotListeners } from "./roomPreview/roomPreviewListeners";
 
 registerRoomPreviewSnapshotListeners();
 
 loadHeadOverHeelsFont();
-loadCodiconFont();
 
 render(
   <Provider store={store}>
