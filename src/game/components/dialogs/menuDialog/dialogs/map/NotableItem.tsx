dd19dc5c04cb60d29a80e00127de44c3deb3e93d
Jim Higson <jim@blockstack.ing>
2026-07-03 21:57:29 +0100

    fix: sprites on the map in Safari now render properly, working around some Safari bugs with foreignObject in svg (#979)

--- src/game/components/dialogs/menuDialog/dialogs/map/NotableItem.tsx
+++ src/game/components/dialogs/menuDialog/dialogs/map/NotableItem.tsx
@@ -9,7 +9,7 @@ import {
   type Xyz,
 } from "../../../../../../utils/vectors/vectors";
 import { usePlayableTailwindSpriteClassname } from "../../../../tailwindSprites/playableTailwindSpriteClassname";
-import { ScrollIntoView } from "./ScrollIntoView";
+import { SvgSpriteInRoom } from "./SvgSpriteInRoom";
 
 /**
  * items that get rendered on the map if they are found in the room's json
@@ -19,47 +19,6 @@ export type NotableItem<RoomId extends string> =
   | JsonItem<"lift", RoomId>
   | JsonItem<"pickup", RoomId>
   | JsonItem<"teleporter", RoomId>;
-
-const SpriteInRoom = ({
-  class: className,
-  scrollTo = false,
-  onClick,
-  yAdjust = 0,
-}: {
-  /**
-   * should have a sprite-* utility class and some way to set the --scale var - for this to actually show a sprite
-   */
-  class: string;
-  scrollTo?: boolean;
-  onClick?: (e: MouseEvent) => void;
-  yAdjust?: number;
-}) => {
-  return (
-    <foreignObject
-      x={-50}
-      y={-100 + yAdjust}
-      width={100}
-      height={100}
-      class={className}
-      // because of its width and height, this will cover up other rooms behind this one,
-      // and will stop clicks on them - eg, in the editor to change room via the map.
-      // however, this may come at the cost of switching by clicking on char on the map in-game
-      style={{ pointerEvents: "none" }}
-    >
-      <div
-        class={`sprite zx:sprite-revert-to-two-tone ml-[calc(50px-var(--scale)*var(--w)*1px/2)] mt-[calc(100px-var(--scale)*var(--h)*1px)] ${
-          className
-        }`}
-        onClick={onClick}
-        // having turned pointer events off for the foreign object, turn them back on for the item
-        // sprite *iff* we have a click handler to call
-        // TODO: test this in iphone Safari, since it struggles with fos!
-        style={onClick ? { pointerEvents: "all" } : {}}
-      />
-      {scrollTo && <ScrollIntoView />}
-    </foreignObject>
-  );
-};
 
 export const PlayableItemInRoom = ({
   characterName,
@@ -78,13 +37,13 @@ export const PlayableItemInRoom = ({
 }) => {
   const spriteClassname = usePlayableTailwindSpriteClassname();
   return (
-    <SpriteInRoom
-      class={`${onlyPlayableInRoom ? "[--scale:2.5]" : "[--scale:1.5]"}
-                ${spriteClassname({
-                  character: characterName,
-                  action: isCurrent ? "walking" : "idle",
-                  facingXy8: vectorClosestDirectionXy8(facing) ?? "towards",
-                })}`}
+    <SvgSpriteInRoom
+      scale={onlyPlayableInRoom ? 2.5 : 1.5}
+      spriteClass={spriteClassname({
+        character: characterName,
+        action: isCurrent ? "walking" : "idle",
+        facingXy8: vectorClosestDirectionXy8(facing) ?? "towards",
+      })}
       scrollTo={isCurrent}
       yAdjust={yAdjust}
       onClick={(e) => {
@@ -98,23 +57,25 @@ export const PlayableItemInRoom = ({
 
 export const NotableItemSvg = ({ item }: { item: NotableItem<string> }) => {
   const isBigItem = item.type === "hushPuppy" || item.type === "teleporter";
-  const scaleClass = isBigItem ? "[--scale:1.25]" : "[--scale:1.5]";
+  // the biggest sprites get scaled down so all sprites are about the same size
+  const scale = isBigItem ? 1.25 : 1.5;
 
   if (item.type === "lift") {
     return (
       <>
-        <SpriteInRoom
-          class={`${scaleClass} ${"texture-lift_static" satisfies TextureTailwindClass}`}
+        <SvgSpriteInRoom
+          scale={scale}
+          spriteClass={"texture-lift_static" satisfies TextureTailwindClass}
         />
-        <SpriteInRoom
-          class={`${scaleClass} ${"texture-lift_2" satisfies TextureTailwindClass}`}
+        <SvgSpriteInRoom
+          scale={scale}
+          spriteClass={"texture-lift_2" satisfies TextureTailwindClass}
         />
       </>
     );
   }
 
   const spriteClassName =
-    // the biggest sprites get scaled down so all sprites are about the same size:
     item.type === "teleporter" ?
       ("texture-teleporter" satisfies TextureTailwindClass)
     : item.type === "hushPuppy" ?
@@ -147,5 +108,5 @@ export const NotableItemSvg = ({ item }: { item: NotableItem<string> }) => {
       : ("texture-crown_bookworld" satisfies TextureTailwindClass)
     : ("texture-block_organic" satisfies TextureTailwindClass);
 
-  return <SpriteInRoom class={`${scaleClass} ${spriteClassName}`} />;
+  return <SvgSpriteInRoom scale={scale} spriteClass={spriteClassName} />;
 };
